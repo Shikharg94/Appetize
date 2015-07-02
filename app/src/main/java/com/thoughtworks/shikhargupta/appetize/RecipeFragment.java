a6c16e6d1d90b5cd6a1c9e0f4a55a5f6f72b607d
Chirag Aggarwal <chi6rag@gmail.com>
2015-07-02 17:22:50 +0530

    Add code for RecipeAdapter

--- app/src/main/java/com/thoughtworks/shikhargupta/appetize/RecipeFragment.java
+++ app/src/main/java/com/thoughtworks/shikhargupta/appetize/RecipeFragment.java
@@ -2,13 +2,18 @@ package com.thoughtworks.shikhargupta.appetize;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * Created by shikhargupta on 02/07/15.
@@ -24,6 +29,9 @@ public class RecipeFragment extends Fragment{
             {"Brownie", "Waffles", "Custard"},
             {"Pizza"}
     };
+    int[][] recipeImages = {
+            {R.drawable.}
+    }
 
     @Override
     public void onAttach(Activity activity) {
@@ -40,8 +48,7 @@ public class RecipeFragment extends Fragment{
         recipeGrid = (GridView) view.findViewById(R.id.recipeGrid);
         int categoryIndex = communicator.respond();
         String[] recipiesList = recipes[categoryIndex];
-        Log.d("chi6rag", recipiesList[0]);
-
+//      Log.d("chi6rag", recipiesList[0]);
 
         return view;
     }
@@ -49,5 +56,76 @@ public class RecipeFragment extends Fragment{
     public interface Communicator {
         public int respond();
     }
+}
 
+class Recipe {
+    int image;
+    String recipeName;
+
+    Recipe(int image, String recipeName){
+        this.image = image;
+        this.recipeName = recipeName;
+    }
+}
+
+class RecipeAdapter extends BaseAdapter {
+
+    Context context;
+    int[] images;
+    String[] recipeNames;
+    ArrayList<Recipe> recipeArrayList;
+
+    RecipeAdapter(Context context, int[] images, String[] recipeNames){
+        this.context = context;
+        this.recipeNames = recipeNames;
+        this.images = images;
+        for(int i = 0; i < recipeNames.length; i++){
+            recipeArrayList.add(i, new Recipe(images[i], recipeNames[i]) );
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return recipeArrayList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return recipeArrayList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    class ViewHolder {
+        ImageView recipeImage;
+        TextView recipeName;
+
+        ViewHolder(View v){
+            recipeImage = (ImageView) v.findViewById(R.id.recipeImage);
+            recipeName  =  (TextView) v.findViewById(R.id.recipeName);
+        }
+
+    }
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View recipeItem  = convertView;
+        ViewHolder holder = null;
+        if(recipeItem==null){
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(
+                    Context.LAYOUT_INFLATER_SERVICE);
+            recipeItem = inflater.inflate(R.layout.single_recipe, parent, false);
+            holder = new ViewHolder(recipeItem);
+            recipeItem.setTag(holder);
+        }
+        else{
+            holder = (ViewHolder) recipeItem.getTag();
+        }
+        holder.recipeImage.setImageResource();
+        return null;
+    }
 }
